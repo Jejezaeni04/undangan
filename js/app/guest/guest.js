140e22636a5e14015977a99b43a893f5cf92b999
Jejezaeni04 <jejezaeni040901@gmail.com>
2025-06-25 23:05:59 +0700

    first commit

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -253,10 +253,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
-            details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            text: 'The Walimatul Khitanan Ghibran Muhammad Al-fatih',
+            dates: `${formatDate('2025-07-03 11:00')}/${formatDate('2025-07-03 11:00')}`,
+            details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara walimatul khitanan anak kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
+            location: 'RT 02 RW 06, Kampung Pedurenan, Kelurahan Pabuaran, Cibinong.',
             ctz: config.get('tz'),
         });
 
